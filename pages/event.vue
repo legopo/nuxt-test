942f72e82904fe29c531523c0ee8db8c2bfd825b
legopo <taraki3594@aol.jp>
2021-06-27 18:55:30 +0900

    event

---
+++ pages/event.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="container">
+    <input type="button" value="Result" @click.once="omikuji" />
+    <p>結果: {{ result }}</p>
+    <hr />
+    <div id="parent" v-on:click="onParentClick">
+      親要素
+      <div id="my" v-on:click="onMyClick">
+        <!-- <div id="my" v-on:click.self="onMyClick"> -->
+        <!-- <div id="my" v-on:click.stop="onMyClick"> -->
+        <!-- <div id="my" v-on:click.self.stop="onMyClick"> -->
+        現在要素
+        <div id="child" v-on:click="onChildClick">
+          <!-- <div id="child" v-on:click.stop="onChildClick"> -->
+          子要素
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      result: "-"
+    };
+  },
+  methods: {
+    omikuji: function(e) {
+      this.result = Math.floor(Math.random() * 100) + 1;
+    },
+    onParentClick: function(e) {
+      console.log('#parent run...');
+    },
+    onMyClick: function(e) {
+      console.log('#my run...');
+    },
+    onChildClick: function(e) {
+      console.log('#child run...');
+    }
+  }
+};
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -4,6 +4,7 @@
       <li><NuxtLink to="/instance">instance</NuxtLink></li>
       <li><NuxtLink to="/component">component</NuxtLink></li>
       <li><NuxtLink to="/bind-customize">bind-customize</NuxtLink></li>
+      <li><NuxtLink to="/event">event</NuxtLink></li>
       <li><NuxtLink to="/about">about</NuxtLink></li>
     </ul>
   </div>
